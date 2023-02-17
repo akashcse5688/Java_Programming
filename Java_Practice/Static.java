5781a7ec0780034bbc4506a6e255889672a3545f
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2023-02-18 00:46:41 +0600

    Some java program practice

---
+++ Java_Practice/Static.java
@@ -0,0 +1,26 @@
+class Student3
+{
+  
+int id;
+   
+String name;
+   
+
+void display ()
+  {
+    System.out.println (id + " " + name);
+  } 
+
+public static void main (String args[])
+  {
+    
+Student3 s1 = new Student3 ();
+    
+Student3 s2 = new Student3 ();
+    
+s1.display ();
+    
+s2.display ();
+
+} 
+} 
