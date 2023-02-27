46ce2f92a1f7c01b2a4e5722477cae5760b786e6
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2023-02-28 01:49:55 +0600

    Main method

---
+++ Lab Class/Lab_class-7/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Vehicle obj1= new Vehicle();
+        obj1.run();
+
+        Bike obj2 =new Bike();
+        obj2.run();
+
+        Car obj3 = new Car();
+        obj3.run();
+        }
+}
