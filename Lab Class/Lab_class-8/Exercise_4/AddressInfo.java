ca80e03e808e97c2f23ef3d98f1d69322ad1042a
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2023-03-09 23:15:00 +0600

    Exercise on aggregation

---
+++ Lab Class/Lab_class-8/Exercise_4/AddressInfo.java
@@ -0,0 +1,11 @@
+public class AddressInfo {
+    String street,city,state,postalCode;
+
+    public AddressInfo(String street, String city,String state,String postalCode)
+    {
+        this.street=street;
+        this.city=city;
+        this.state=state;
+        this.postalCode=postalCode;
+    }
+}
