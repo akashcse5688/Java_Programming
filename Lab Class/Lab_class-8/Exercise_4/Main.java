86b771cd3590e105c43f21c60a8bcaf2b37dd4a7
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2023-03-10 15:38:07 +0600

    Update Main.java

--- Lab Class/Lab_class-8/Exercise_4/Main.java
+++ Lab Class/Lab_class-8/Exercise_4/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         AddressInfo addressInfo= new AddressInfo("Khagan", "Daffodil Smart city", "Dhaka", "1711");
         InsuranceInfo insuranceInfo =new InsuranceInfo("Life Insurance", 1234);
-        Employeee employeee =new Employeee("Bappy",addressInfo, insuranceInfo );
+        Employeee employeee =new Employeee("AKASH",addressInfo, insuranceInfo );
         employeee.display();
     }
 }
