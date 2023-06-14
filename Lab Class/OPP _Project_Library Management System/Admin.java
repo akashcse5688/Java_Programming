77ed9a3a69e1c004d12b3bdd3f3d3ea0004259d2
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2023-06-15 00:16:41 +0600

    Create Interface that created  of issued books, add books, return books and book details method

---
+++ Lab Class/OPP _Project_Library Management System/Admin.java
@@ -0,0 +1,13 @@
+public interface Admin {
+    //The Admin Menu is designed to show details of users, issued books, add books, return books
+    void addBook();
+
+    void issueBook();
+
+    void returnBOOK();
+
+    void detail();
+
+    void exit();
+
+}
